024813b34ca1022c4e823df842e0118ff03044b6
Nick Krenowicz <n2143_sites2@outlook.com>
2017-06-09 11:40:12 -0700

    echo not printing \n fix
    Added -e option to process expressions

--- setupScripts/newserver.sh
+++ setupScripts/newserver.sh
@@ -8,12 +8,12 @@
 # ensure executable  chmod +x $HOME/bin/BasicLinuxScripts/setupScripts/newserver.sh
 # ./$HOME/bin/BasicLinuxScripts/setupScripts/newserver.sh
 
-echo "Starting Update\n"
+echo -e "Starting Update\n"
 sudo apt-get update
-echo "\n-----upgrade------"
+echo -e "\n-----upgrade------"
 sudo apt-get -y upgrade
 
-echo "\n-----java install------" #http://askubuntu.com/questions/464755/how-to-install-openjdk-8-on-14-04-lts
+echo -e "\n-----java install------" #http://askubuntu.com/questions/464755/how-to-install-openjdk-8-on-14-04-lts
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
 #set auto accept license
@@ -22,29 +22,29 @@ sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y oracle-java8-set-default
 java -version
 
-echo "\n-----program installs------"
-sudo apt-get install -y inxi htop screen git build-essential cmake
+echo -e "\n-----program installs------"
+sudo apt-get install -y inxi htop screen git build-essential
 sudo apt-get install -y openssh-server openssh-client
 # Other packages to note: ubuntu-restricted-extras cmake
-echo "\n------update"
+echo -e "\n------update"
 sudo apt-get update
-echo "\n------upgrade------"
+echo -e "\n------upgrade------"
 sudo apt-get -y upgrade
-echo "\n------autoremove------"
+echo -e "\n------autoremove------"
 sudo apt-get -y autoremove
 
-echo "\n------Updating path------"
+echo -e "\n------Updating path------"
 sudo export PATH=$PATH:$HOME/bin
 #Create sybolic link for update script.  PATH variable is setup only to $HOME/bin
 ln -s $HOME/bin/BasicLinuxScripts/update $HOME/bin/update
 
 #This was probably already setup
-echo "\n------Creating User Bin------"
+echo -e "\n------Creating User Bin------"
 mkdir $HOME/bin
 
-echo "\n------Configuring git------"
+echo -e "\n------Configuring git------"
 git config --global user.email n2143_sites2@outlook.com
 git config --global user.name "Nick Krenowicz"
-
-echo "\n\nRecommend to reboot"
+echo -e "------Done------"
+echo -e "\n\nRecommend to reboot"
 echo "Then do sudo apt-get dist-upgrade  if desired"
